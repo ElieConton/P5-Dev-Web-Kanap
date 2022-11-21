f75c6aeaf13b00dab1f49aaf5166eac7dfe38f56
ElieConton <elieconton@gmail.com>
2022-11-21 21:11:41 +0100

    Affichage Item page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,39 @@
-let t = new URL(window.location.href)
-let o = t.searchParams.get('id')
-console.log(o)
+let originalLink = new URL(window.location.href)
+let idLink = originalLink.searchParams.get('id')
+let productLink = 'http://localhost:3000/api/products/' + idLink
 
-let u = 'http://localhost:3000/api/products/' + o
-console.log(u)
+
+
+
+function displayItemSelected (item) {
+    const image = document.createElement('img')
+        document.querySelector('div.item__img').append(image)
+        image.src = `${item.imageUrl}`
+        image.alt = `${item.altTxt}`
+    const title = document.querySelector('#title')
+        title.innerText = `${item.name}` 
+    const price = document.querySelector('#price')
+        price.innerText = `${item.price}`
+    const description = document.querySelector('#description')
+        description.innerText = `${item.description}`
+    const colors = `${item.colors}`
+    const colorsArray = colors.split(',')
+    const colorWrapper = document.querySelector('#colors')
+    colorsArray.forEach(function(colorItem){
+        const optionColor = new Option (colorItem, colorItem)
+        colorWrapper.options[colorWrapper.options.length] = optionColor 
+    })
+}
+
+async function main () {
+    const r = await fetch(productLink , {
+        headers: {
+        Accept: 'application/json'
+        }
+    })
+    const item = await r.json()
+    displayItemSelected(item)
+}
+ main()
+
+ 
