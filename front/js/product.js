0eab491e0a49e5967e00945461e1ecdb067e8f44
ElieConton <elieconton@gmail.com>
2022-11-22 08:47:18 +0100

    ajout commentaire

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,14 @@
+
+//Récupération id produit pour interroger l'API uniquement sur le produit sélectionné 
 let originalLink = new URL(window.location.href)
 let idLink = originalLink.searchParams.get('id')
 let productLink = 'http://localhost:3000/api/products/' + idLink
 
-
+/**
+ * Crée du contenu text et HTML pour représenter l'objet sélectionné
+ * @param {{imageUrl: string, altText: string, name: string, description: string, colors: array of string}} item 
+ * 
+ */
 
 
 function displayItemSelected (item) {
@@ -23,6 +29,7 @@ function displayItemSelected (item) {
         const optionColor = new Option (colorItem, colorItem)
         colorWrapper.options[colorWrapper.options.length] = optionColor 
     })
+    return
 }
 
 async function main () {
