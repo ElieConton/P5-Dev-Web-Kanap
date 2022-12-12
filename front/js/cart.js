3222250328390fca130c7322d907841717d46658
ElieConton <elieconton@gmail.com>
2022-12-12 10:46:00 +0100

    gestion suppression et changement panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -42,47 +42,7 @@ function displayItemsInCart(informationFromCart, informationFromLink) {
   document.querySelector("#cart__items").appendChild(article);
 
 }
- /* const div3 = document.createElement("div");
-  div2.appendChild(div3);
-  div3.classList = "cart__item__content__description";
-  const h2 = document.createElement("h2");
-  div3.appendChild(h2);
-  h2.innerText = `${informationFromLink.name}`;
-  const p = document.createElement("p");
-  div3.appendChild(p);
-  p.innerText = informationFromCart.color;
-  const p1 = document.createElement("p");
-  div3.appendChild(p1);
-  p1.innerText = `${informationFromLink.price} €`;
-  const div4 = document.createElement("div");
-  div2.appendChild(div4);
-  div4.classList = "cart__item__content__settings";
-  const div5 = document.createElement("div");
-  div4.appendChild(div5);
-  div5.classList = "cart__item__content__settings__quantity";
-  const p2 = document.createElement("p");
-  div5.appendChild(p2);
-  p2.innerText = `Qté: `;
-  const input = document.createElement("input");
-  input.addEventListener("change", (e) => {
-    console.log(e);
-  });
-  div5.appendChild(input);
-  input.type = "number";
-  input.classList = "itemQuantity";
-  input.name = "itemQuantity";
-  input.min = "1";
-  input.max = "100";
-  input.value = `${informationFromCart.quantity}`;
-  const div6 = document.createElement("div");
-  div4.appendChild(div6);
-  div6.classList = "cart__item__content__settings__delete";
-  const p3 = document.createElement("p");
-  div4.appendChild(p3);
-  p3.classList = "deleteItem";
-  p3.innerText = "Supprimer";
-}*/
-
+ 
 function makeImage(product) {
   const imageItem = document.createElement("div");
   imageItem.classList = "cart__item__img";
@@ -96,20 +56,20 @@ function makeImage(product) {
 function cartItemContent(productFromCart, productFromLink) {
   const itemContent = document.createElement("div");
   itemContent.classList = "cart__item__content"
-  itemContent.appendChild(cartItemContentDescription(productFromLink))
+  itemContent.appendChild(cartItemContentDescription(productFromCart, productFromLink))
   itemContent.appendChild(cartItemContentSettings(productFromCart))
   return itemContent
 }
 
-function cartItemContentDescription(product) {
+function cartItemContentDescription(productFromCart, productFromLink) {
   const itemContentDescription = document.createElement("div")
   itemContentDescription.classList = "cart__item__content__description"
   const name = document.createElement("h2")
-  name.innerText = `${product.name}`
+  name.innerText = `${productFromLink.name}`
   const color = document.createElement("p")
-  color.innerText = `${product.color}`
+  color.innerText = `${productFromCart.color}`
   const price = document.createElement("p")
-  price.innerText = `${product.price}`
+  price.innerText = `${productFromLink.price} €`
   itemContentDescription.appendChild(name)
   itemContentDescription.appendChild(color)
   itemContentDescription.appendChild(price)
@@ -125,7 +85,10 @@ function cartItemContentSettings(product) {
   quantity.innerText = "Qté : "
   const input = document.createElement("input")
   input.addEventListener("change", (e) => {
-    console.log(e);
+    const itemArticle = itemContentSettings.closest('article')
+    const cartItem =  cart.find((p) => p.id === itemArticle.dataset.id && p.color === itemArticle.dataset.color);
+    cart[cart.indexOf(cartItem)].quantity = input.value
+    saveCart(cart)
   });
   input.type = "number";
   input.classList = "itemQuantity";
@@ -137,6 +100,13 @@ function cartItemContentSettings(product) {
   itemContentSettingsDelete.classList = "cart__item__content__settings__delete"
   const itemDelete = document.createElement("p")
   itemDelete.innerText = "Supprimer"
+  itemDelete.addEventListener("click", (e) => {
+    const itemArticle = itemContentSettings.closest('article')
+    itemArticle.remove()
+    const cartItem =  cart.find((p) => p.id === itemArticle.dataset.id && p.color === itemArticle.dataset.color);
+    cart.splice(cart.indexOf(cartItem), 1 )
+    saveCart(cart)
+  });
   itemContentSettings.appendChild(itemContentSettingsQuantity)
   itemContentSettings.appendChild(itemContentSettingsDelete)
   itemContentSettingsQuantity.appendChild(quantity)
